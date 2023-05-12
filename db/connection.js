a6e1752e28a2a5a683a509346cdb0a1298d44534
VeronikaD14 <3smile4me@gmail.com>
2023-05-12 14:29:56 +0100

    hosst 4 errors

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,15 @@
 const { Pool } = require('pg');
 const ENV = process.env.NODE_ENV || 'development';
 
+require('dotenv').config({
+  path: `${__dirname}/../.env.${ENV}`,
+});
+console.log(ENV)
+
+
+if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
+  throw new Error('PGDATABASE or DATABASE_URL not set');}
+
 const config =
   ENV === 'production'
     ? {
@@ -9,12 +18,8 @@ const config =
       }
     : {};
 
-require('dotenv').config({
-  path: `${__dirname}/../.env.${ENV}`,
-});
-console.log(ENV)
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
- throw new Error('PGDATABASE or DATABASE_URL not set');}
+  throw new Error('PGDATABASE or DATABASE_URL not set');}
 
 module.exports = new Pool(config);
